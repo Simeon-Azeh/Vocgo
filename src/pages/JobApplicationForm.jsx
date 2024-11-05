94d3b16b4d4ec1420291e0aaaadf033c87a3c8b9
JuniorDCoder <foryoungjuniorngu@gmail.com>
2024-11-05 12:06:30 +0100

    feat: More changes

--- src/pages/JobApplicationForm.jsx
+++ src/pages/JobApplicationForm.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 import { GoArrowLeft } from "react-icons/go";
+import SuccessModal from '../components/SuccessModal';
 
 function JobApplicationForm() {
-
   const user = JSON.parse(localStorage.getItem('user'));
   const { jobId } = useParams();
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     fullName: user.name,
     email: user.email,
     resume: null,
     linkedin: ''
   });
+  const [isModalVisible, setModalVisible] = useState(false);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -25,14 +27,19 @@ function JobApplicationForm() {
     e.preventDefault();
     console.log('Job applied:', formData);
     // Handle form submission (e.g., send data to API)
+    setModalVisible(true);
+    setTimeout(() => {
+      setModalVisible(false);
+      navigate('/dashboard');
+    }, 5000);
   };
 
   return (
     <div className="max-w-2xl p-6 mx-auto mt-8 bg-white rounded font-inter">
-        <Link className='flex items-center gap-2 mb-4 text-gray-800' to='/jobs'>
-       <GoArrowLeft />
-       Go back
-       </Link>
+      <Link className='flex items-center gap-2 mb-4 text-gray-800' to='/jobs'>
+        <GoArrowLeft />
+        Go back
+      </Link>
       <h2 className="mb-4 text-2xl font-semibold text-gray-800">Apply for Job ID: {jobId}</h2>
       <form onSubmit={handleSubmit}>
         <div className="mb-4">
@@ -84,6 +91,15 @@ function JobApplicationForm() {
           Submit Application
         </button>
       </form>
+      <SuccessModal 
+        isVisible={isModalVisible} 
+        onClose={() => {
+          setModalVisible(false);
+          navigate('/dashboard');
+        }} 
+        title="Application Submitted" 
+        message="You have successfully applied for the job. We will get back to you soon!" 
+      />
     </div>
   );
 }
